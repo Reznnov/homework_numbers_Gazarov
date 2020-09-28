934ccf666e98ec7e9af3bf256e6d7446ac9e00e7
Reznnov <gazarovgg@gmail.com>
2020-09-28 09:31:03 +0300

    ffff

---
+++ ft_additional_code.py
@@ -0,0 +1,58 @@
+def ft_bin_num(a):
+    if a < 0:
+        a *= -1
+    desit = 1
+    it = 0
+    while a > 0:
+        it += a % 2 * desit
+        desit *= 10
+        a //= 2
+    return it
+
+
+def ft_len_num(x):
+    i = 0
+    while x > 0:
+        x = x // 10
+        i += 1
+    return i
+
+
+def ft_straight_code(y):
+    dvoi = ft_bin_num(y)
+    dlin = ft_len_num(dvoi)
+    if y < 0:
+        dvoi = dvoi + 10000000
+        return dvoi
+    else:
+        return dvoi
+
+
+def ft_reverse_code(q):
+    prum = ft_straight_code(q)
+    pov = prum
+    chis = 0
+    dell = 1
+    while pov > 0:
+        if (prum // dell) % 10 == 1:
+            prum = prum - (1 * dell)
+        elif (prum // dell) % 10 == 0:
+            prum = prum + (1 * dell)
+        pov = pov // 10
+        dell *= 10
+    return prum
+
+
+def ft_additional_code(w):
+    nov = ft_reverse_code(w)
+    itog = nov + 1
+    check = itog
+    ten = 1
+    while check > 0:
+        if check % 10 == 2:
+            itog = itog - (1 * ten)
+            itog = itog + (1 * ten * 10)
+        check //= 10
+        ten *= 10
+    return itog
+        
